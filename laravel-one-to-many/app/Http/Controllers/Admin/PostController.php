1f69517b3949cc4a8d0e91f4c6c07e46dcfd555f
Leonomics <leocv97@gmail.com>
2022-10-21 17:07:52 +0200

    first commit

---
+++ laravel-one-to-many/app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->limit(50)->get();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::orderBy('name', 'asc')->get();
+        return view('admin.posts.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $params = $request->validate([
+            'title' => 'required|max:255|min:5',
+            'content' => 'required',
+            'category_id' => 'nullable|exists:categories,id'
+        ]);
+
+        $params['slug'] = Post::getUniqueSlugFrom($params['title']);
+
+        $post = Post::create($params);
+
+        return redirect()->route('admin.posts.show', $post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::orderBy('name', 'asc')->get();
+        return view('admin.posts.edit', compact('categories', 'post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        // dd($request->all());
+
+        $params = $request->validate([
+            'title' => 'required|max:255|min:5',
+            'content' => 'required',
+            'category_id' => 'nullable|exists:categories,id'
+        ]);
+
+        if ($params['title'] !== $post->title) {
+            $params['slug'] = Post::getUniqueSlugFrom($params['title']);
+        }
+
+        $post->update($params);
+
+        return redirect()->route('admin.posts.show', $post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+}
---
+++ laravel-one-to-many/resources/views/admin/posts/show.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+  <div class="row">
+    <div class="col-8">
+      <h1>{{ $post->title }}</h1>
+      <p>{{ $post->slug }}</p>
+
+      @if($post->category)
+      <p><strong>Categoria:</strong> {{ $post->category->name }}</p>
+      @endif
+
+      <ul class="d-flex gap-2">
+        <li>{{ $post->created_at }}</li>
+        <li>{{ $post->updated_at }}</li>
+      </ul>
+    </div>
+    <div class="col-4 text-left d-flex justify-content-end align-items-center">
+      <a href="{{ route('admin.posts.edit',$post) }}" type="button" class="btn btn-primary btn-sm">Modifica</a>
+      <form action="{{ route('admin.posts.destroy',$post) }}" method="POST">
+      
+        @csrf
+        @method('DELETE')
+
+        <input type="submit" value="Elimina" class="btn btn-danger btn-sm">
+      </form>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <p>
+        {!! $post->content !!}
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <ul class="col-12">
+      @if($post->category)
+        @foreach($post->category->posts as $relatedPost)
+
+          <li>{{ $relatedPost->title }}</li>
+        @endforeach
+      @endif
+    </ul>
+  </div>
+</div>
+
+@endsection
